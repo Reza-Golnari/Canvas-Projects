0d4a5062cadc951aed3efe4d8bb6afd79e40a48b
Reza <i.am.reza.programmer@gmail.com>
2024-02-18 21:21:53 +0330

    Create Project

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+}
+</style>
